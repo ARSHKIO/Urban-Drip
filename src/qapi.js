c29340526338929e94f90c228287cf04ecc26331
ARSHKIO <77559417+ARSHKIO@users.noreply.github.com>
2021-01-17 08:03:06 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60039e65daacc2001e8f7ba5",
+				"60039e65daacc2001e8f7ba7"
+			]
+		},
+		"60039e65daacc2001e8f7ba5": {
+			"id": "60039e65daacc2001e8f7ba5",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60039e65daacc2001e8f7ba7": {
+			"id": "60039e65daacc2001e8f7ba7",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
